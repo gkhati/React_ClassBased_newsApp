7d27031178fce9bc97bea77bf536059a131fdc11
gkhati <97231451+gkhati@users.noreply.github.com>
2022-04-07 16:56:50 +0530

    React

---
+++ src/components/News.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import NewsItems from './NewsItems'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+          hjjhgjg
+          <NewsItems/>
+      </div>
+    )
+  }
+}
+
+export default News
+
+
---
+++ src/components/NewsItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItems extends Component {
+  render() {
+    return (
+      <div>
+          bnvnfg
+      </div>
+    )
+  }
+}
+
+export default NewsItems
