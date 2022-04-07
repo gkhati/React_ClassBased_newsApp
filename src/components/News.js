2f0a8117153bb60cb6cebe816b3f402299ce4cc4
gkhati <97231451+gkhati@users.noreply.github.com>
2022-04-07 17:36:00 +0530

    React

--- src/components/News.js
+++ src/components/News.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 
 export class News extends Component {
-  render() {
-    return (
-      <div>
-          hjjhgjg
-          <NewsItems/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='container my-3'>
+                <h2>News App- Headlines</h2>
+                <div className='row my-3'>
+                    <div className="col-md-4 ">
+                        <NewsItems title="my_title" description="Data to show" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItems title="my_title" description="Data to show" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItems title="my_title" description="Data to show" />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 
 export class NewsItems extends Component {
-  render() {
-    return (
-      <div>
-          bnvnfg
-      </div>
-    )
-  }
+    render() {
+        let {title, description}=this.props;    //Destructuing concept if this.props is a object then 
+        // pull data as title and description and make it available
+        return (
+            <div>
+                <div className="card" style={{width: "18rem"}}>
+                    <img src="https://cdn.24.co.za/files/Cms/General/d/663/93a5c0dda0554249a1f8690ca0d8b6f8.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href="/newsDetail" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default NewsItems
