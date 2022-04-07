e52a877d72a8d2b57ca0bbf79d6e7c95426d994d
gkhati <97231451+gkhati@users.noreply.github.com>
2022-04-07 23:08:41 +0530

    React

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 
 export class NewsItems extends Component {
+     //constructor(){
+        // super();
+      //   console.log("Its constructor") //called three times because constructor of NewsItem
+         // state of card can be set here
+    // }
+
     render() {
-        let {title, description}=this.props;    //Destructuing concept if this.props is a object then 
+        let {title, description, imageUrl,newsUrl}=this.props;    //Destructuing concept if this.props is a object then 
         // pull data as title and description and make it available
         return (
             <div>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src="https://cdn.24.co.za/files/Cms/General/d/663/93a5c0dda0554249a1f8690ca0d8b6f8.jpg" className="card-img-top" alt="..." />
+                    <img src={imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/newsDetail" className="btn btn-primary">Go somewhere</a>
+                        <a href="/newsDetail" className="btn btn-sm  btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
